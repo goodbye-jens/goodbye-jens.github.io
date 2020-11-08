233f72f8e74aafa2f3f8caa1ca0822384b2985bd
Lucas Werkmeister <mail@lucaswerkmeister.de>
2020-11-08 18:02:54 +0100

    Initial commit
    Some basic layout, up to the “get a compliment” button (which doesn’t do
    anything yet), and the code to load the Commons Picture of the Day as
    the background image and show the appropriate attribution.
    
    Written as a single HTML file for simplicity, at least for now.
    Indented with tabs; the Emacs .dir-locals.el file sets up everything to
    be indented with two-wide tabs, though we can of course add config for
    other editors later, if needed. (I manually removed the “align”
    indentation from the ternary operator in lines 121/122, since I couldn’t
    figure out how to make Emacs indent them by only one level.)

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((mhtml-mode . ((js-indent-level . 2)
+                (css-indent-offset . 2)
+                (sgml-basic-offset . 2)
+                (indent-tabs-mode . t)
+                (tab-width . 2))))
